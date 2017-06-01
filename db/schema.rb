cd6b6e712a50a15d642649f32960e7de412d341d
Carl Eqladios <ceqladios@gmail.com>
2017-06-01 05:29:44 +0200

    Migrate DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170601004626) do
+ActiveRecord::Schema.define(version: 20170601030925) do
+
+  create_table "chapters", force: :cascade do |t|
+    t.integer  "number"
+    t.string   "name"
+    t.string   "image"
+    t.boolean  "finished"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
