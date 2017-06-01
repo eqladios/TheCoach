4fe154123b7d621b42d279c7786a087d37021541
Carl Eqladios <ceqladios@gmail.com>
2017-06-01 09:28:22 +0200

    Add Seed File to Populate DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Chapter.create!(number: '1', name: 'Introduction')
+Chapter.create!(number: '2', name: 'Time Complexity')
+Chapter.create!(number: '3', name: 'Sorting')
+Chapter.create!(number: '4', name: 'Data Structures')
+Chapter.create!(number: '5', name: 'Complete Search')
+User.create!(email: "admin@admin.com",
+             password:              "password",
+             password_confirmation: "password",
+             admin: true)
+User.create!(email: "user@user.com",
+            password:              "password",
+            password_confirmation: "password")
