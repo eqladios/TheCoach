c2a17b5e1a8b33a7440a570d024eac3056b4bbef
Carl Eqladios <ceqladios@gmail.com>
2017-06-01 04:41:23 +0200

    Configurations for Heroku and PG

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,5 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  # For Devise on Heroku
   config.action_mailer.default_url_options = { host: 'carlcoach.herokuapp.com' }
+  config.assets.initialize_on_precompile = false
 end
