47914948adfdb91a2fc57b4ac242390f7713d762
Carl Eqladios <ceqladios@gmail.com>
2017-06-02 01:19:09 +0200

    Uncomment seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,18 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# Chapter.create!(number: '1', name: 'Introduction')
-# Chapter.create!(number: '2', name: 'Time Complexity')
-# Chapter.create!(number: '3', name: 'Sorting')
-# Chapter.create!(number: '4', name: 'Data Structures')
-# Chapter.create!(number: '5', name: 'Complete Search')
-# User.create!(email: "admin@admin.com",
-#              password:              "password",
-#              password_confirmation: "password",
-#              admin: true)
-# User.create!(email: "user@user.com",
-#             password:              "password",
-#             password_confirmation: "password")
+Chapter.create!(number: '1', name: 'Introduction')
+Chapter.create!(number: '2', name: 'Time Complexity')
+Chapter.create!(number: '3', name: 'Sorting')
+Chapter.create!(number: '4', name: 'Data Structures')
+Chapter.create!(number: '5', name: 'Complete Search')
+User.create!(email: "admin@admin.com",
+             password:              "password",
+             password_confirmation: "password",
+             admin: true)
+User.create!(email: "user@user.com",
+            password:              "password",
+            password_confirmation: "password")
 #Chapter 1
 Topic.create!(number: '1', title: 'Programming Languages', htmlContent: '', chapter_id: '1')
 Topic.create!(number: '2', title: 'Input And Output', htmlContent: '', chapter_id: '1')
